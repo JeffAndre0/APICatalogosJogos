4035e4f9a4821a9420575904c362a772ca88942a
Jefferson André <jefferson.andre96@gmail.com>
2021-07-17 18:12:56 -0300

    Adicionar arquivos de projeto.

---
+++ Services/IJogoServices.cs
@@ -0,0 +1,21 @@
+﻿using APICatalogosJogos.InputModel;
+using APICatalogosJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogosJogos.Services
+{
+    public interface IJogoServices : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+
+
+    }
+}
